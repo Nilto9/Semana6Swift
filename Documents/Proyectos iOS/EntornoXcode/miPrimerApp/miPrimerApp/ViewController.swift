69e94907a198f98fda4d837db7c40a7444d3103c
Nilton <palian.nilton@hotmail.com>
2022-05-06 18:18:25 -0500

    first commit

---
+++ Documents/Proyectos iOS/EntornoXcode/miPrimerApp/miPrimerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  miPrimerApp
+//
+//  Created by MAC12 on 8/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
